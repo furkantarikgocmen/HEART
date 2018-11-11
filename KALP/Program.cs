8c058bdadc5397c3b08cfb381db97b42a02a0732
furkantarikgocmen <f.tarikgocmen@outlook.com>
2018-11-11 19:08:45 +0300

    UPDATE

---
+++ KALP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KALP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen İsim Giriniz,-Baş harf büyük-");
+            string kelime = Console.ReadLine();
+            int sayi = kelime.Length;
+
+            for (int i = 0, m = 1; i < 30; i++)
+                for (int l = 0; l < new[] { 5, 6, 7, 6, 8, 10, 3, 10, 4, 13, 1, 13, 1, 87, 1, 27, 4, 23, 7, 20, 11, 16, 16, 11, 20, 7, 24, 3, 27, 1 }[i]; l++, m++)
+                    Console.Write((i % 2 > 0 ? kelime [m % sayi] : ' ') + (m % 29 > 0 ? "" : "\n"));
+            Console.ReadKey();
+        }
+    }
+}
